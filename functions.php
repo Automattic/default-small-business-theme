1b17066a0370a6994a177f61ecd22247785a8785
Danny Dudzic <danieldudzic@gmail.com>
2018-08-28 19:18:04 +0200

    Adds the Gutenberg Starter Theme files with a changed prefix

---
+++ functions.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Gutenbergtheme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Gutenbergtheme
+ */
+
+if ( ! function_exists( 'smallbusinesstheme_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function smallbusinesstheme_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on smallbusinesstheme, use a find and replace
+		 * to change 'smallbusinesstheme' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'smallbusinesstheme', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'menu-1' => esc_html__( 'Primary', 'smallbusinesstheme' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+
+		// Adding support for core block visual styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Add support for full and wide align images.
+		add_theme_support( 'align-wide' );
+
+		// Add support for custom color scheme.
+		add_theme_support( 'editor-color-palette', array(
+			array(
+				'name'  => __( 'Strong Blue', 'smallbusinesstheme' ),
+				'slug'  => 'strong-blue',
+				'color' => '#0073aa',
+			),
+			array(
+				'name'  => __( 'Lighter Blue', 'smallbusinesstheme' ),
+				'slug'  => 'lighter-blue',
+				'color' => '#229fd8',
+			),
+			array(
+				'name'  => __( 'Very Light Gray', 'smallbusinesstheme' ),
+				'slug'  => 'very-light-gray',
+				'color' => '#eee',
+			),
+			array(
+				'name'  => __( 'Very Dark Gray', 'smallbusinesstheme' ),
+				'slug'  => 'very-dark-gray',
+				'color' => '#444',
+			),
+		) );
+	}
+endif;
+add_action( 'after_setup_theme', 'smallbusinesstheme_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function smallbusinesstheme_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'smallbusinesstheme_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'smallbusinesstheme_content_width', 0 );
+
+/**
+ * Register Google Fonts
+ */
+function smallbusinesstheme_fonts_url() {
+	$fonts_url = '';
+
+	/*
+	 *Translators: If there are characters in your language that are not
+	 * supported by Noto Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$notoserif = esc_html_x( 'on', 'Noto Serif font: on or off', 'smallbusinesstheme' );
+
+	if ( 'off' !== $notoserif ) {
+		$font_families = array();
+		$font_families[] = 'Noto Serif:400,400italic,700,700italic';
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return $fonts_url;
+
+}
+
+/**
+ * Enqueue scripts and styles.
+ */
+function smallbusinesstheme_scripts() {
+	wp_enqueue_style( 'gutenbergbase-style', get_stylesheet_uri() );
+
+	wp_enqueue_style( 'smallbusinessthemeblocks-style', get_template_directory_uri() . '/css/blocks.css' );
+
+	wp_enqueue_style( 'smallbusinesstheme-fonts', smallbusinesstheme_fonts_url() );
+
+	wp_enqueue_script( 'smallbusinesstheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'smallbusinesstheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'smallbusinesstheme_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
---
+++ template-parts/content.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Smallbusinesstheme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			if ( has_post_thumbnail() ) :
+				the_post_thumbnail();
+			endif;
+		endif;
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php smallbusinesstheme_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'smallbusinesstheme' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'smallbusinesstheme' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php smallbusinesstheme_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
