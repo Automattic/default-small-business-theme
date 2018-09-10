460ccd7d1f66c8ad81e291f8fdea85515cdbc55b
Allan Cole <allan.cole@automattic.com>
2018-09-10 18:14:25 -0400

    Add comment styling

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -100,3 +100,58 @@ if ( ! function_exists( 'smallbusinesstheme_entry_footer' ) ) :
 		);
 	}
 endif;
+
+if ( ! function_exists( 'smallbusinesstheme_comment' ) ) :
+
+function smallbusinesstheme_comment($comment, $args, $depth) {
+    if ( 'div' === $args['style'] ) {
+        $tag       = 'div';
+        $add_below = 'comment';
+    } else {
+        $tag       = 'li';
+        $add_below = 'div-comment';
+    }?>
+    <<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID() ?>"><?php
+    if ( 'div' != $args['style'] ) { ?>
+        <div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
+    } ?>
+        <div class="comment-author vcard"><?php
+            if ( $args['avatar_size'] != 0 ) {
+                echo get_avatar( $comment, $args['avatar_size'] );
+            }
+            printf( __( '<cite class="fn">%s</cite>' ), get_comment_author_link() ); ?>
+        </div><?php
+        if ( $comment->comment_approved == '0' ) { ?>
+            <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php
+        } ?>
+        <div class="comment-meta commentmetadata">
+            <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>"><?php
+                /* translators: 1: date, 2: time */
+                printf(
+                    __('%1$s at %2$s'),
+                    get_comment_date(),
+                    get_comment_time()
+                ); ?>
+            </a><?php
+            edit_comment_link( __( '(Edit)' ), '  ', '' ); ?>
+        </div>
+
+        <?php comment_text(); ?>
+
+        <div class="reply"><?php
+                comment_reply_link(
+                    array_merge(
+                        $args,
+                        array(
+                            'add_below' => $add_below,
+                            'depth'     => $depth,
+                            'max_depth' => $args['max_depth']
+                        )
+                    )
+                ); ?>
+        </div><?php
+    if ( 'div' != $args['style'] ) : ?>
+        </div><?php
+    endif;
+}
+endif;
