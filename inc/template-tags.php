a7cc57ff80355d78aa7b0ca5871a7dcce9b03b60
Allan Cole <allan.cole@automattic.com>
2018-09-11 12:28:12 -0400

    Add post navigation template tag and styles

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -103,7 +103,7 @@ endif;
 
 if ( ! function_exists( 'smallbusinesstheme_comment' ) ) :
 
-function smallbusinesstheme_comment($comment, $args, $depth) {
+	function smallbusinesstheme_comment($comment, $args, $depth) {
     if ( 'div' === $args['style'] ) {
         $tag       = 'div';
         $add_below = 'comment';
@@ -154,4 +154,27 @@ function smallbusinesstheme_comment($comment, $args, $depth) {
         </div><?php
     endif;
 }
+endif;
+
+if ( ! function_exists( 'smallbusinesstheme_the_post_navigation' ) ) :
+
+	function smallbusinesstheme_the_post_navigation() {
+
+		// Previous/next post navigation.
+		$next_post = get_next_post();
+		$previous_post = get_previous_post();
+		$next_post_content = get_post_field( 'post_content', $next_post->ID);
+		$previous_post_content = get_post_field( 'post_content', $previous_post->ID);
+
+		the_post_navigation( array(
+			'next_text' => '<span class="screen-reader-text">' . __( 'Next post:', 'smallbusinesstheme' ) . '</span> ' .
+			'<h2 class="post-title">%title</h2>' .
+			'<p>' . wp_trim_words( $next_post_content , '28' ) . '</p>' .
+			'<button>' . __( 'Next', 'smallbusinesstheme' ) . '</button>',
+			'prev_text' => '<span class="screen-reader-text">' . __( 'Previous post:', 'smallbusinesstheme' ) . '</span> ' .
+			'<h2 class="post-title">%title</h2>' .
+			'<p>' . wp_trim_words( $previous_post_content , '28' ) . '</p>' .
+			'<button>' . __( 'Previous', 'smallbusinesstheme' ) . '</button>',
+		) );
+	}
 endif;
--- single.php
+++ single.php
@@ -16,10 +16,14 @@ get_header(); ?>
 
 		get_template_part( 'template-parts/content', get_post_type() );
 
+/*
 		the_post_navigation( array(
 			'prev_text' => '← %title',
 			'next_text' => '%title →',
 		) );
+*/
+
+		smallbusinesstheme_the_post_navigation();
 
 		// If comments are open or we have at least one comment, load up the comment template.
 		if ( comments_open() || get_comments_number() ) :
