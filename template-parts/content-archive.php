48b04f6c5fafe0e12ece9c405291eb6e9da0f8cd
Allan Cole <allan.cole@automattic.com>
2018-09-19 18:42:53 -0400

    Add archive content-type, improve archive class logic, cleanup block styling, code tidying

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -25,6 +25,8 @@
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php smallbusinesstheme_entry_footer(); ?>
+		<div class="entry-meta">
+			<?php smallbusinesstheme_entry_footer(); ?>
+		</div>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -24,7 +24,7 @@
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h2 class="entry-title">', '</h2>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
