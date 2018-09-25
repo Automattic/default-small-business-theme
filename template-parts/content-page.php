33c8c64f592d0576233ae192a862644d7639a2a7
Allan Cole <allan.cole@automattic.com>
2018-09-25 13:00:03 -0400

    Add minor fixes to entry-meta line heights and text area font-sizes

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php if ( get_the_title() ) { ?>
 	<header class="entry-header">
 		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 	</header><!-- .entry-header -->
+	<?php } ?>
 
 	<div class="entry-content">
 		<?php
