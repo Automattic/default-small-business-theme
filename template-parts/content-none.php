e43e536a21b1b240f22c96ee0deee32e94ec8fb1
alaczek <ola@bodera.com>
2018-10-16 16:30:55 +1100

    Style page headers and 404 page.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -11,7 +11,7 @@
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'smallbusinesstheme' ); ?></h1>
+		<h2 class="page-title"><?php esc_html_e( 'Nothing Found', 'smallbusinesstheme' ); ?></h2>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
