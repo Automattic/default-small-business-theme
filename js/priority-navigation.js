c0f30facffa78e13624d3f827b75b33ac7cdec45
Ernesto Méndez <mendezcode@gmail.com>
2018-10-22 16:23:08 -0400

    Fix priority navigation.

--- js/priority-navigation.js
+++ js/priority-navigation.js
@@ -1,100 +1,49 @@
 /**
- * Priority plus menu pattern.
- *
- * src: https://gist.github.com/sarahmonster/e99db050ffdb14ac4296fc76a23d7e56
+ * Priority navigation.
  */
 
 ( function( $ ) {
+	var menu = $( '#primary-menu' );
+	var more = $( '#more-menu' );
+	var hidden = more.find( 'ul.sub-menu' );
 
-	// Priority+ navigation, whee!
-	function priorityNav() {
-
-		var mainNav  = $( '#site-navigation' ),
-			moreMenu = $( '#more-menu' );
-
-		// Make sure we have a menu and that the more-more item is present
-		if ( 0 < mainNav.length && 0 < moreMenu.length ) {
-			var navWidth         = 0,
-				firstMoreElement = moreMenu.find( 'li' ).first();
-
-			// Calculate the width of our "more" containing element
-			var moreWidth = moreMenu.outerWidth( true );
-
-			// Calculate the current width of our navigation
-			mainNav.find( '> div > ul > li' ).each( function() {
-				navWidth += $( this ).outerWidth( true );
-			});
-
-			// Calculate our available space
-			var availableSpace = mainNav.outerWidth( true ) - moreWidth;
-
-			// If our nav is wider than our available space, we're going to move items
-			if ( navWidth > availableSpace ) {
-				var lastItem = mainNav.find( '> div > ul > li:not(#more-menu)' ).last();
-
-				lastItem.attr( 'data-width', lastItem.outerWidth( true ) );
-				lastItem.prependTo( moreMenu.find( '.sub-menu, .children' ).eq( 0 ) );
-				// Rerun this function!
-				setTimeout( priorityNav, 10 );
-
-			// But if we have the extra space, we should add the items back to our menu
-			} else if ( navWidth + firstMoreElement.data( 'width' ) < availableSpace ) {
-				// Check to be sure there's enough space for our extra element
-				firstMoreElement.insertBefore( mainNav.find( '> div > ul > li' ).last() );
-				setTimeout( priorityNav, 10 );
+	function adjustMenu() {
+		hideItems() || showItems();
+	}
+	
+	function hideItems() {
+		var w = 0, width = menu.outerWidth() - more.outerWidth(), didHide = false;
+		var itemsHidden = hidden.children( 'li' ).length > 0;
+		menu.children( 'li' ).not( more ).each( function( i, node ) {
+			var item = $( node );
+			w += item.outerWidth();
+			if ( w > width ) {
+				itemsHidden ? item.prependTo( hidden ) : item.appendTo( hidden );;
+				didHide = true;
 			}
-
-			// Hide our more-menu entirely if there's nothing in it
-			if ( moreMenu.find( 'ul li' ).length > 0 ) {
-				moreMenu.addClass( 'visible' );
-			} else {
-				moreMenu.removeClass( 'visible' );
-			}
-
-		} // check for body class
-	} // function priorityNav
-
-	// Debounce
-	function debounce(func, wait = 20, immediate = true) {
-		var timeout;
-
-		return function() {
-			var context = this, args = arguments;
-			var later = function() {
-				timeout = null;
-				if (!immediate) {
-					func.apply(context, args);
-				}
-			};
-			var callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) {
-				func.apply(context, args);
-			}
-		};
+		} );
+		return didHide;
 	}
 
-	// Run our functions once the window has loaded fully
-	$( window ).on( 'load', debounce( function() {
-		priorityNav();
-		setTimeout( function() {
-			$( '.js #site-navigation' ).addClass('show-menu');
-		}, 200 );
-	}));
+	function showItems() {
+		var width = menu.outerWidth() - more.outerWidth();
+		var visibleWidth = menu.children( 'li' ).not( more ).toArray().reduce( function( w, node ) {
+			return w + $( node ).outerWidth();
+		}, 0 );
+		hidden.children( 'li' ).each( function( i, node ) {
+			var item = $( node ), w = item.outerWidth();
+			if ( ( visibleWidth + w ) < width ) {
+				item.insertBefore( more );
+				visibleWidth += w;
+			}
+		} );
+	}
+	
+	function adjustMenuHandler() {
+		if ( window.innerWidth >= 815 ) adjustMenu();
+	}
+	
+	$( window ).resize( adjustMenuHandler );
 
-	// Annnnnd also every time the window resizes
-	var isResizing = false;
-	$( window ).on( 'resize', function() {
-		if (isResizing) {
-			return;
-		}
-
-		isResizing = true;
-		setTimeout( function() {
-			priorityNav();
-			isResizing = false;
-		}, 150 );
-	});
-
-} )( jQuery );
+	adjustMenuHandler();
+} ).call( this, jQuery );
